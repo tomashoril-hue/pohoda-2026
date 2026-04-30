f20af959e18bcc2bcbb8b59e8da17367d8dd21d8
Tomáš Horil <tomas.horil@gmail.com>
2026-04-30 02:13:02 +0200

    improve group members list

--- app/dashboard/group/page.tsx
+++ app/dashboard/group/page.tsx
@@ -39,7 +39,8 @@ export default async function GroupPage() {
           id,
           email,
           meno,
-          priezvisko
+          priezvisko,
+          telefon
         )
       `)
       .eq('group_id', membership.group_id)
